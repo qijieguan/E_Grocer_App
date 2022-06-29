ea9d373ac86453e347bdcfbaa92f242c63012028
qijieguan <qijieguan7@gmail.com>
2022-06-29 13:16:16 -0700

    fixed routing url display

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -8,63 +8,63 @@ export class ItemService {
 
   DEFAULT_LIST: any[] = [
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://images.pexels.com/photos/6555515/pexels-photo-6555515.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       name: 'Tomato',
       description: 'Fresh and bright red. Please don\'t throw me.',
       quantity: 1,
       price: 1.99,
-      reviews: [{content: "It is rotten. Don\'t order.",  name: 'anonymous'}],
+      reviews: [{content: "It was rotten. Don\'t buy it. I had to sue the company to receive compensation.",  name: 'anonymous'}],
       hide_quantity: false,
     },
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://images.pexels.com/photos/8446853/pexels-photo-8446853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       name: 'Mango',
       description: 'Yellow Tropical fruit. Packed with nutrients and protective antioxidants.',
       quantity: 1,
       price: 2.99,
-      reviews: [],
+      reviews: [{content: "10/10. Very delicious and I am addicted to mangos!",  name: 'anonymous'}],
       hide_quantity: false,
     },
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/05/spinach-1296x728-header.jpg?w=1155&h=1528',
       name: 'Spinach',
       description: 'Leafy green flowering plant from Asia. Eat your daily vegetables.',
       quantity: 1,
       price: 1.50,
-      reviews: [],
+      reviews: [{content: "7.5/10. Came in warm. Would have loved it if it was refrigerated on delivery.",  name: 'anonymous'}],
       hide_quantity: false,
     },
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg',
       name: 'Potato',
       description: 'Hungry? Just bake or fry up some potatoes.',
       quantity: 1,
       price: 1.39,
-      reviews: [],
+      reviews: [{content: "Loved it! Great value for its price.",  name: 'anonymous'}],
       hide_quantity: false,
     },
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://images.pexels.com/photos/301669/pexels-photo-301669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       name: 'Cinnamon',
       description: 'A spice extracted from bark of cinnamon trees. Used in various cooking. Do not consume by itself.',
       quantity: 1,
       price: 2.59,
-      reviews: [],
+      reviews: [{content: "8/10. Very happy with the product!",  name: 'anonymous'}],
       hide_quantity: false,
     },
     {
-      _id: "",
+      _id: uuidv4(),
       url: 'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       name: 'Red Meat',
       description: 'Fresh raw meat. Keep up with your protein diet, bro.',
       quantity: 1,
       price: 8.39,
-      reviews: [],
+      reviews: [{content: "10/10. The package was delivered to me cold!",  name: 'anonymous'}],
       hide_quantity: false,
     }
   ]
@@ -75,7 +75,7 @@ export class ItemService {
     let i: number = 0;
 
     if (!sessionStorage.getItem("unique_ids")) {
-      for (let i = 0; i < this.DEFAULT_LIST.length; ++i) { UNIQUE_IDS.push(uuidv4()); }
+      this.DEFAULT_LIST.forEach(x => { UNIQUE_IDS.push(x._id); } );
       sessionStorage.setItem("unique_ids", JSON.stringify(UNIQUE_IDS));
     }
     else {
