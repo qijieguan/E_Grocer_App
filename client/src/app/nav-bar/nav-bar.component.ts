d87cc48177c958768509eb8ebc99ab5c52869d7c
qijieguan <qijieguan7@gmail.com>
2022-09-18 22:22:34 -0700

    added observer for text animation

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -12,6 +12,11 @@ export class NavBarComponent implements OnInit {
   private cartNum: number = 0;
 
   constructor(private location: Location, private cart_service: CartService) {
+  }
+
+  getCartNum = () => { return this.cartNum; }
+
+  ngOnInit(): void {
     this.location.onUrlChange(url => {
       setTimeout(()=> { window.scrollTo({top: 0, behavior: 'smooth'}); }, 125); 
 
@@ -23,6 +28,7 @@ export class NavBarComponent implements OnInit {
       let bg_image = document.querySelector(".app-wrapper");
 
       document.getElementsByTagName('header')[0].classList.remove('home');
+    
       header?.classList.remove('grocery_list-link', 'cart-link', 'note-link', 'background');
       nav?.classList.remove('fixed');
 
@@ -47,15 +53,25 @@ export class NavBarComponent implements OnInit {
 
       document.querySelector('.highlight')?.classList.remove('highlight');
       document.querySelector('.' + querySelect)?.classList.add('highlight');
-
+      setTimeout(() => { this.activeObserver(); }, 500);
     });
 
     this.cart_service.getCartItem().subscribe((cart) => { this.cartNum = cart.length; });
   }
 
-  getCartNum = () => { return this.cartNum; }
+  activeObserver = () => {
+    const faders = document.querySelectorAll('.fade-slide');
+    
+    const appearOptions = { threshold: 0, rootMargin: '0px 0px 0px 0px' }; 
 
-  ngOnInit(): void {
+    const appearOnScroll = new IntersectionObserver (
+      function( entries ) {
+          entries.forEach(entry => {
+              if (entry.isIntersecting) { entry.target.classList.add('appear'); }
+          });
+      },
+    appearOptions);  
+
+    faders.forEach(fader => { appearOnScroll.observe(fader); });
   }
-
 }
